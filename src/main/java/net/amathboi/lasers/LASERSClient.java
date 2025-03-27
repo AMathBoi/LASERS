719e29e921c1abdd2b5a8870422093d2d2fbd907
AMathBoi <acomputerman@proton.me>
2025-03-27 17:12:05 -0400

    Initial commit

---
+++ src/main/java/net/amathboi/lasers/LASERSClient.java
@@ -0,0 +1,10 @@
+package net.amathboi.lasers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LASERSClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
